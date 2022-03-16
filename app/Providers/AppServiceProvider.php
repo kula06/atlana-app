2e0a17da41f92c33d1c538760531bd36ca0dd0be
Alex <alex@cloudfeel.ru>
2022-03-16 20:18:24 +0200

    added force https on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \Schema::defaultStringLength(191);
+
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
